21435bcf11e8d1a920949d164f7e1c44faa2dfee
Peter Mitri <peter.mitri@rte-france.com>
2025-01-06 17:23:58 +0100

    remove docker server (doesn't always work)

--- docker-serve.sh
+++
@@ -1,2 +0,0 @@
-docker build -t rte-france.github.io:latest -f docker/ubuntu22.dockerfile docker
-docker run -p 127.0.0.1:4000:4000 --mount type=bind,source=$(pwd),target=/home/rte/rte-france.github.io rte-france.github.io:latest
--- docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-setfacl -R -m u:rte:rwx /home/rte/rte-france.github.io
-exec runuser -u rte -- "$@"
